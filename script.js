fdf573de6dd3922107f0db512fa83d404adf34c0
nikaraa <giuseppemalara00001@gmail.com>
2023-03-23 00:02:17 +0100

    Write some functions to display

--- script.js
+++ script.js
@@ -1,30 +1,85 @@
-const operatorButton= document.querySelector('.operator-button');
-const numberButton= document.querySelector('.number-button');
-const deleteButton= document.querySelector('.delete-button');
-const deleteAllButton= document.querySelector('.delete-all');
-const equalsButton= document.querySelector('.equals');
-const display=document.querySelector('.result');
-const lastNumbers=document.querySelector('.last-digits')
+const operatorButton = document.querySelectorAll('.operator-button')
+const numberButton = document.querySelectorAll('.number-button')
+const deleteButton = document.querySelector('.delete-button')
+const deleteAllButton = document.querySelector('.delete-all')
+const equalsButton = document.querySelector('.equals')
+const displayDiv = document.querySelector('.result')
+const lastNumbers = document.querySelector('.last-digits')
 
+let firstNumber = ''
+let secondNumber = ''
+let operator = null
 
-let firstNumber = '';
-let secondNumber = '';
-let operator;
+deleteAllButton.addEventListener('click', resetAll())
+
+numberButton.forEach((button) =>
+    button.addEventListener('click', () => pressNumber(button.textContent))
+)
+
+operatorButton.forEach((button) =>
+    button.addEventListener('click', () => pressOperation(button.textContent))
+)
+
+equalsButton.addEventListener('click', doOperation())
+deleteButton.addEventListener('click', deleteButtonFunction())
+
+function deleteButtonFunction(string){
+    string.slice(string.length-1);
+}
+function resetAll() {
+    displayDiv.textContent = '0'
+    lastNumbers.textContent = ''
+    firstNumber = ''
+    secondNumber = ''
+    operator = null
+}
+
+function resetScreen() {
+    displayDiv.innerHTML = ''
+}
+
+function pressNumber(number) {
+    if (displayDiv.textContent === '0') {
+        resetScreen()
+    }
+    display = number;
+    displayDiv.textContent += display
+}
+
+function pressOperation(operation) {
+    if (operator === null) {
+        firstNumber = displayDiv.textContent
+        operator = operation
+        lastNumbers.textContent = firstNumber+"" + operator
+        resetScreen()
+    }
+    else if (operator != null) {
+        doOperation()
+    }
+}
+
+function doOperation() {
+    if(operation===null){
+        return;
+    }
+    secondNumber = displayDiv.textContent
+    displayDiv.textContent= operate(operation, firstNumber, secondNumber)
+}
 
 function add(a, b) {
-    return a + b;
+    return a + b
 }
 
 function subtract(a, b) {
-    return a - b;
+    return a - b
 }
 
 function multiply(a, b) {
-    return a * b;
+    return a * b
 }
 
 function divide(a, b) {
-    return a / b;
+    return a / b
 }
 
 function operate(operator, a, b) {
@@ -32,6 +87,13 @@ function operate(operator, a, b) {
         case '+': return add(a, b)
         case '-': return subtract(a, b)
         case '*': return multiply(a, b)
-        case '/': return divide(a, b)
+        case '/': {
+            if (b === 0) {
+                return;
+            }
+            else {
+                return divide(a, b)
+            }
+        }
     }
 }
